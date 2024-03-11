624ff9e51a681d0a3d3a4df3e70fa24a7bd9aa5b
tonyccm2 <tonyccm2@gmail.com>
2024-03-11 11:19:54 -0500

    menu svg

--- src/pages/home/Base.tsx
+++ src/pages/home/Base.tsx
@@ -1,5 +1,6 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import emoticon from '../../assets/emoticon.svg';
+import menu from '../../assets/menu.svg';
 
 export const Base = () => {
   const { state } = useLocation();
@@ -7,17 +8,23 @@ export const Base = () => {
   return (
     <div>
       <div className='flex justify-center'>
+        <button type="button" className='min-w-6'>
+          <img className='my-auto mx-3' src={menu} alt='menu' />
+        </button>
         <div className='flex w-full max-w-screen-lg justify-center'>
           {
-            picture
-              ? <img className='w-6 h-6 rounded-full my-auto' src={picture} alt="icon perfil" />
-              : <img src={emoticon} alt='perfil' />
+            !picture
+              ? <img className='w-12 h-12 rounded-full my-auto mx-6' src={picture} alt="icon perfil" />
+              : <img className='my-auto mx-3' src={emoticon} alt='perfil' />
           }
-          <h1 className='text-white text-2xl font-bold p-2 truncate ... w-2/3 md:w-auto'>{name}</h1>
+          <h1 className='text-white text-2xl font-bold p-2 truncate ... w-auto'>{name}</h1>
         </div>
+        <div className='p-6'></div>
       </div>
-      <div className='flex w-full max-w-screen-lg justify-center'>
-        <Outlet />
+      <div className='flex justify-center'>
+        <div className='max-w-screen-lg '>
+          <Outlet />
+        </div>
       </div>
     </div>
   )
